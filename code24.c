728d7f398df246af78d5b7840e9e8f68939560c6
Nikhilverma007621 <nikhilvermaanilsdt123@gmail.com>
2026-02-12 23:31:19 +0530

    Add files via upload

---
+++ code24.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    int m, n;
+
+    scanf("%d %d", &m, &n);
+
+    int matrix[m][n];
+
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    bool isToeplitz = true;
+
+    for(int i = 1; i < m; i++) {
+        for(int j = 1; j < n; j++) {
+            if(matrix[i][j] != matrix[i-1][j-1]) {
+                isToeplitz = false;
+                break;
+            }
+        }
+        if(!isToeplitz)
+            break;
+    }
+
+    if(isToeplitz)
+        printf("true");
+    else
+        printf("false");
+
+    return 0;
+}
